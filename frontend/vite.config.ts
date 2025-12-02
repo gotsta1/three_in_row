4a537d86f0c753c449547ced34eff7af6e42dfc5
gotsta1 <a3unnn@gmail.com>
2025-12-03 00:41:34 +0500

    added for deploy on railway

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const allowedHosts = [
+  'localhost',
+  '127.0.0.1',
+  'frontend-production-3779.up.railway.app',
+];
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
+    allowedHosts,
   },
 });
